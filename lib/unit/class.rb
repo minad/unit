f894a29f6353206a6c43b8ffc75505a431c61972
Grant Hutchins & Ryan Ong <pair+grant+rong@pivotallabs.com>
2012-01-06 12:37:18 -0500

    Stop using coerce in arithmetic and logic methods

--- lib/unit/class.rb
+++ lib/unit/class.rb
@@ -44,20 +44,48 @@ class Unit < Numeric
   end
 
   def *(other)
-    a, b = coerce(other)
-    Unit.new(a.value * b.value, a.unit + b.unit, system)
+    case other
+    when Unit
+      Unit.new(other.value * self.value, other.unit + self.unit, system)
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self * other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   def /(other)
-    a, b = coerce(other)
-    Unit.new(Integer === a.value && Integer === b.value ? Rational(b.value, a.value) : b.value / a.value,
-             b.unit + Unit.power_unit(a.unit, -1), system)
+    case other
+    when Unit
+      new_value = if Integer === self.value && Integer === other.value
+                    Rational(self.value, other.value)
+                  else
+                    self.value / other.value
+                  end
+      new_unit = self.unit + Unit.power_unit(other.unit, -1)
+      Unit.new(new_value, new_unit, system)
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self / other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   def +(other)
-    raise TypeError, "#{inspect} and #{other.inspect} are incompatible" if !compatible?(other)
-    a, b = coerce(other).map{|unit| unit.normalize}
-    Unit.new(a.value + b.value, b.unit, system).in(self)
+    case other
+    when Unit
+      raise TypeError, "#{inspect} and #{other.inspect} are incompatible" if !compatible?(other)
+      a = self.normalize
+      b = other.normalize
+      Unit.new(a.value + b.value, b.unit, system).in(self)
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self + other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   def **(exp)
@@ -66,7 +94,18 @@ class Unit < Numeric
   end
 
   def -(other)
-    self + (-other)
+    case other
+    when Unit
+      raise TypeError, "#{inspect} and #{other.inspect} are incompatible" if !compatible?(other)
+      a = self.normalize
+      b = other.normalize
+      Unit.new(a.value - b.value, b.unit, system).in(self)
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self - other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   def -@
@@ -82,13 +121,43 @@ class Unit < Numeric
   end
 
   def ==(other)
-    a, b = coerce(other).map{|unit| unit.normalize}
-    a.value == b.value && a.unit == b.unit
+    case other
+    when Unit
+      a = self.normalize
+      b = other.normalize
+      a.value == b.value && a.unit == b.unit
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self == other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
+  end
+
+  def eql?(other)
+    case other
+    when Unit
+      self.unit == other.unit && self.value.eql?(other.value)
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self.eql? other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   def <=>(other)
-    a, b = coerce(other).map{|unit| unit.normalize}
-    b.value <=> a.value if a.unit == b.unit
+    case other
+    when Unit
+      a = self.normalize
+      b = other.normalize
+      a.value <=> b.value if a.unit == b.unit
+    when Numeric
+      other_unit = Unit.to_unit(other, system)
+      self <=> other_unit
+    else
+      apply_through_coercion(other, __method__)
+    end
   end
 
   # Number without dimension
@@ -242,6 +311,23 @@ class Unit < Numeric
     end
 
     self
+  end
+
+  # Given another object and an operator, use the other object's #coerce method
+  # to perform the operation.
+  #
+  # Based on Matrix#apply_through_coercion
+  def apply_through_coercion(obj, oper)
+    coercion = obj.coerce(self)
+    raise TypeError unless coercion.is_a?(Array) && coercion.length == 2
+    first, last = coercion
+    if first.respond_to?(:public_send)
+      first.public_send(oper, last)
+    else
+      first.send(oper, last)
+    end
+  rescue
+    raise TypeError, "#{obj.inspect} can't be coerced into #{self.class}"
   end
 
   class<< self
