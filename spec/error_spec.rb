4c0cec88fa9faff7ca09b7ad7ee61dd32acfd364
Grant Hutchins & Ryan Ong <pair+grant+rong@pivotallabs.com>
2012-01-06 10:57:21 -0500

    Convert to RSpec
    Add failing spec for Github issue #12

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
-require 'bacon'
-require 'unit'
-require 'unit/dsl'
+require 'spec_helper'
 
 describe "Errors" do
   describe "TypeError when adding incompatible units" do
     it "should have a nice error message" do
       a = Unit(1, "meter")
       b = Unit(1, "second")
-      lambda do
-        a + b
-      end.should.raise(TypeError).message.should.equal("#{a.inspect} and #{b.inspect} are incompatible")
+      lambda { a + b }.should raise_error(TypeError, "#{a.inspect} and #{b.inspect} are incompatible")
     end
   end
 
   describe "TypeError when trying to convert incompatible unit using #in!" do
     it "should have a nice error message" do
       unit = Unit(1000, "m / s")
-      lambda do
+      lambda {
         unit.in!("seconds")
-      end.should.raise(TypeError).message.should.equal(%{Unexpected Unit("1000/1 m.s^-1"), expected to be in s})
-      lambda do
+      }.should raise_error(TypeError, %{Unexpected Unit("1000/1 m.s^-1"), expected to be in s})
+      lambda {
         unit.in_seconds!
-      end.should.raise(TypeError).message.should.equal(%{Unexpected Unit("1000/1 m.s^-1"), expected to be in s})
+      }.should raise_error(TypeError, %{Unexpected Unit("1000/1 m.s^-1"), expected to be in s})
     end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require 'rspec'
+require 'unit'
+require 'unit/dsl'
